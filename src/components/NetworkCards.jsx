cad63901e02cd7bb8121435508d48da14857381c
jahanvi208 <jazzbuzz@gmail.com>
2024-11-17 00:05:47 +0530

    mou logos added

--- src/components/NetworkCards.jsx
+++ src/components/NetworkCards.jsx
@@ -6,8 +6,8 @@ function NetworkCards() {
             <div style={{backgroundColor:'#131412'}} className="p-4 rounded-lg flex flex-col justify-center items-center my-3 w-72">
                 <img src="kiet.jpg" alt="Asana" className="" />
                 <div className='text-center my-2'>
-                    <h2 className="text-2xl mb-1 font-semibold">Figma</h2>
-                    <p className="text-l text-zinc-400">Notion is a versatile all-in-one workspace application</p>
+                    <h2 className="text-2xl mb-1 font-semibold">KIET </h2>
+                    <p className="text-l text-zinc-400">KIET strives to create technically competent professionals who can contribute productively towards the betterment of the Industry as well as the Society.</p>
                 </div>
             </div>
         </div>
@@ -19,8 +19,8 @@ function NetworkCards1() {
             <div style={{backgroundColor:'#131412'}} className="p-4 rounded-lg flex flex-col justify-center items-center my-3 w-72">
                 <img src="dtu.png" alt="Asana" className="" />
                 <div className='text-center my-2'>
-                    <h2 className="text-2xl mb-1 font-semibold">Figma</h2>
-                    <p className="text-l text-zinc-400">Notion is a versatile all-in-one workspace application</p>
+                    <h2 className="text-2xl mb-1 font-semibold">DTU</h2>
+                    <p className="text-l text-zinc-400">DTU has an illustrious history of providing quality education and promoting research and entrepreneurship for over eight decades.</p>
                 </div>
             </div>
         </div>
@@ -32,8 +32,8 @@ function NetworkCards2() {
             <div style={{backgroundColor:'#131412'}} className="p-4 rounded-lg flex flex-col justify-center items-center my-3 w-72">
                 <img src="ims.jpg" alt="Asana" className="" />
                 <div className='text-center my-2'>
-                    <h2 className="text-2xl mb-1 font-semibold">Figma</h2>
-                    <p className="text-l text-zinc-400">Notion is a versatile all-in-one workspace application</p>
+                    <h2 className="text-2xl mb-1 font-semibold">IMS </h2>
+                    <p className="text-l text-zinc-400">Empowering minds through innovative learning, fostering holistic development and shaping future leaders</p>
                 </div>
             </div>
         </div>
@@ -44,8 +44,8 @@ function NetworkCards2() {
             <div style={{backgroundColor:'#131412'}} className="p-4 rounded-lg flex flex-col justify-center items-center my-3 w-72">
                 <img src="surajmal.jpg" alt="Asana" className="" />
                 <div className='text-center my-2'>
-                    <h2 className="text-2xl mb-1 font-semibold">Figma</h2>
-                    <p className="text-l text-zinc-400">Notion is a versatile all-in-one workspace application</p>
+                    <h2 className="text-2xl mb-1 font-semibold">Maharaja Surajmal</h2>
+                    <p className="text-l text-zinc-400">Where academic excellence meets innovation, and success is just the beginning</p>
                 </div>
             </div>
         </div>
@@ -56,8 +56,8 @@ function NetworkCards2() {
             <div style={{backgroundColor:'#131412'}} className="p-4 rounded-lg flex flex-col justify-center items-center my-3 w-72">
                 <img src="bennett.jpg" alt="Asana" className="" />
                 <div className='text-center my-2'>
-                    <h2 className="text-2xl mb-1 font-semibold">Figma</h2>
-                    <p className="text-l text-zinc-400">Notion is a versatile all-in-one workspace application</p>
+                    <h2 className="text-2xl mb-1 font-semibold">Bennett University</h2>
+                    <p className="text-l text-zinc-400">Where innovation meets excellence, shaping the leaders of tomorrow.</p>
                 </div>
             </div>
         </div>
@@ -68,8 +68,8 @@ function NetworkCards2() {
             <div style={{backgroundColor:'#131412'}} className="p-4 rounded-lg flex flex-col justify-center items-center my-3 w-72">
                 <img src="rlac.png" alt="Asana" className="" />
                 <div className='text-center my-2'>
-                    <h2 className="text-2xl mb-1 font-semibold">Figma</h2>
-                    <p className="text-l text-zinc-400">Notion is a versatile all-in-one workspace application</p>
+                    <h2 className="text-2xl mb-1 font-semibold">Ram Lal Anand </h2>
+                    <p className="text-l text-zinc-400">Where knowledge meets curiosity, and dreams take fligh</p>
                 </div>
             </div>
         </div>
@@ -81,8 +81,8 @@ function NetworkCards6() {
             <div style={{backgroundColor:'#131412'}} className="p-4 rounded-lg flex flex-col justify-center items-center my-3 w-72">
                 <img src="kirori.jpg" alt="Asana" className="" />
                 <div className='text-center my-2'>
-                    <h2 className="text-2xl mb-1 font-semibold">Figma</h2>
-                    <p className="text-l text-zinc-400">Notion is a versatile all-in-one workspace application</p>
+                    <h2 className="text-2xl mb-1 font-semibold">Kirori Mal College</h2>
+                    <p className="text-l text-zinc-400">Empowering minds, enriching lives, and shaping futures since 1954</p>
                 </div>
             </div>
         </div>
@@ -94,8 +94,8 @@ function NetworkCards7() {
             <div style={{backgroundColor:'#131412'}} className="p-4 rounded-lg flex flex-col justify-center items-center my-3 w-72">
                 <img src="kolhapur.jpg" alt="Asana" className="" />
                 <div className='text-center my-2'>
-                    <h2 className="text-2xl mb-1 font-semibold">Figma</h2>
-                    <p className="text-l text-zinc-400">Notion is a versatile all-in-one workspace application</p>
+                    <h2 className="text-2xl mb-1 font-semibold">Kolhapur Institute Of Technology</h2>
+                    <p className="text-l text-zinc-400">Where innovation meets excellence, shaping tomorrow's technologists toda</p>
                 </div>
             </div>
         </div>
@@ -107,8 +107,8 @@ function NetworkCards8() {
             <div style={{backgroundColor:'#131412'}} className="p-4 rounded-lg flex flex-col justify-center items-center my-3 w-72">
                 <img src="shivaji.png" alt="Asana" className="" />
                 <div className='text-center my-2'>
-                    <h2 className="text-2xl mb-1 font-semibold">Figma</h2>
-                    <p className="text-l text-zinc-400">Notion is a versatile all-in-one workspace application</p>
+                    <h2 className="text-2xl mb-1 font-semibold">Shivaji College</h2>
+                    <p className="text-l text-zinc-400">Illuminating knowledge, inspiring innovation, and empowering generations</p>
                 </div>
             </div>
         </div>
@@ -120,8 +120,8 @@ function NetworkCards9() {
             <div style={{backgroundColor:'#131412'}} className="p-4 rounded-lg flex flex-col justify-center items-center my-3 w-72">
                 <img src="aryabhatta.jpg" alt="Asana" className="" />
                 <div className='text-center my-2'>
-                    <h2 className="text-2xl mb-1 font-semibold">Figma</h2>
-                    <p className="text-l text-zinc-400">Notion is a versatile all-in-one workspace application</p>
+                    <h2 className="text-2xl mb-1 font-semibold">Aryabhatta , Delhi University</h2>
+                    <p className="text-l text-zinc-400">Nurturing minds, fostering innovation, and shaping futures with academic excellence</p>
                 </div>
             </div>
         </div>
@@ -133,8 +133,8 @@ function NetworkCards10() {
             <div style={{backgroundColor:'#131412'}} className="p-4 rounded-lg flex flex-col justify-center items-center my-3 w-72">
                 <img src="jss.jpg" alt="Asana" className="" />
                 <div className='text-center my-2'>
-                    <h2 className="text-2xl mb-1 font-semibold">Figma</h2>
-                    <p className="text-l text-zinc-400">Notion is a versatile all-in-one workspace application</p>
+                    <h2 className="text-2xl mb-1 font-semibold">JSS Academy</h2>
+                    <p className="text-l text-zinc-400">Empowering students, enriching lives, and shaping a brighter future through quality education</p>
                 </div>
             </div>
         </div>
